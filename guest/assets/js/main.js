6811dcc9c0f6004a14c24e956cabf08bfeab96d9
cuonvc <cuong.fithau@gmail.com>
2022-07-17 20:48:34 +0700

    click btn to go the login form

--- guest/assets/js/main.js
+++ guest/assets/js/main.js
@@ -1,5 +1,6 @@
 // form login and register event
 
+var modalForm = document.querySelector(".modal");
 var toRegister = document.querySelector(".modal_to-register");
 var toLogin = document.querySelector(".modal_to-login");
 var toForgetPass = document.querySelector(".forget_pass__submit");
@@ -8,6 +9,8 @@ var login = document.querySelector(".modal-content_login");
 var forgetPass = document.querySelector(".modal-content_forget-pass");
 var backToForm = document.querySelectorAll(".modal_back_submit");
 var formsClose = document.querySelectorAll(".modal_close_submit");
+var loginBtnNav = document.querySelector(".navbar-signin_btn");
+var loginBtnEndPage = document.querySelector(".content-continute_signin");
 
 toRegister.onclick = function() {
     login.style.display = "none";
@@ -37,6 +40,14 @@ for (i = 0; i < backToForm.length; i++) {
 
 for(i = 0; i < formsClose.length; i++) {
     formsClose[i].onclick = function() {
-        document.querySelector(".modal").style.display = "none";
+        modalForm.style.display = "none";
     }
+}
+
+loginBtnNav.onclick = function() {
+    modalForm.style.display = "flex";
+}
+
+loginBtnEndPage.onclick = function() {
+    modalForm.style.display = "flex";
 }
