7d796b16c0762d7ab6497c9f8918d5c99f8cb2c1
cuonvc <cuong.fithau@gmail.com>
2022-08-01 17:56:25 +0700

    Add Fetch get API guest page detail post

--- guest/assets/js/data.js
+++ guest/assets/js/data.js
@@ -1,4 +1,5 @@
 var postsApi = 'http://localhost:8080/api/v1/posts';
+var postByIdApi = 'http://localhost:8080/api/v1/post/20';
 
 start();
 
@@ -7,7 +8,93 @@ function start() {
         renderPostsPin(posts);
         renderPostsNew(posts);
     });
+
+    getPostById(function(post) {
+        renderPostById(post);
+    })
 }
+
+
+// Content post by ID
+
+function getPostById(callback) {
+    fetch(postByIdApi)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(callback);
+}
+
+function renderPostById(postById) {
+    const titlePage = document.getElementById("title-post_guest");
+    const categoryBox = document.querySelector(".body-post_category-list");
+    const titleBox = document.querySelector(".body-post_title");
+    const postAuthBox = document.querySelector(".body-post_auth");
+    const postContentBox = document.querySelector(".body-post_content");
+    const commentListBox = document.querySelector(".comment-list");
+    
+    // console.log(postById);
+
+    titlePage.innerHTML = postById.title;
+
+    let firstCategory = `<li class="body-post_cateogry-item"><a href="#">${postById.categories[0].name}</a></li>`;
+    categoryBox.innerHTML = firstCategory;
+
+    let titleHtml = `<h1 class="post_title--text">${postById.title}</h1>`;
+    titleBox.innerHTML = titleHtml;
+
+    var postCreDate = formatDate(postById.createdDate);
+    let postAuthHtml = `
+        <div class="body-post_auth-avt">
+            <a href="#">
+                <img src="${postById.userProfile.avatarPhoto}" alt="avt">
+            </a>
+        </div>
+        <div class="body-post_auth-name">
+            <a href="#">${postById.userProfile.firstName} ${postById.userProfile.lastName}</a>
+            <span>${postCreDate}</span>
+        </div>
+    `;
+    postAuthBox.innerHTML = postAuthHtml;
+
+    let contentHtmls = `${postById.content}`;
+    postContentBox.innerHTML = contentHtmls;
+
+    let commentList = "";
+    var length = postById.comments.length;
+    
+    for (var i = 0; i < length; i++) {
+        var commentCreDate = formatDate(postById.comments[i].createdDate);
+        var commentMofDate = formatDate(postById.comments[i].modifiedDate);
+        console.log(postById.comments[i].modifiedDate)
+        let commentItem = `
+            <div class="comment-item">
+                <div class="comment-item_auth row no-gutters">
+                    <div class="comment-item_avt">
+                        <a href="#">
+                            <img src="${postById.comments[i].userProfile.avatarPhoto}" alt="">
+                        </a>
+                    </div>
+                    <div class="comment-item_info">
+                        <a href="#">${postById.comments[i].userProfile.firstName} ${postById.comments[i].userProfile.lastName}</a>
+                        <span class="row no-gutters">
+                            <p class="comment-item_info-create">${commentCreDate}</p>
+                            &nbsp;(Chỉnh sửa:&nbsp;<p class="comment-item_info-modify">${commentMofDate}</p>)
+                        </span>
+                    </div>
+                </div>
+                <div class="comment-item_content">
+                    <p>${postById.comments[i].content}</p>
+                </div>
+            </div>
+        `;
+        commentList += commentItem;
+    }
+    commentListBox.innerHTML = commentList;
+}
+
+
+// content list posts
 
 function getPosts(callback) {
     fetch(postsApi)
@@ -23,8 +110,7 @@ function renderPostsPin(posts) {
 
     arrPosts.forEach(post => {
         var firstCategory = post.categories[0];
-        var datePost = new Date(post.createdDate);
-        var finalDate = datePost.getDate() + " th" + (datePost.getMonth() + 1) + ", " + datePost.getFullYear();
+        var finalDate = formatDate(post.createdDate);
         let html = 
         `
             <div class="row">
@@ -76,8 +162,7 @@ function renderPostsNew(posts) {
 
     arrPosts.forEach(post => {
         var firstCategory = post.categories[0];
-        var datePost = new Date(post.createdDate);
-        var finalDate = datePost.getDate() + " th" + (datePost.getMonth() + 1) + ", " + datePost.getFullYear();
+        var finalDate = formatDate(post.createdDate);
         let html = `
             <div class="col l-3 m-4 s-12">
                 <div class="content-post_new">
@@ -115,4 +200,9 @@ function renderPostsNew(posts) {
 
     let contentBox = document.querySelector(".content-posts");
     contentBox.innerHTML = htmls;
+}
+
+function formatDate(dateString) {
+    var dateView = new Date(dateString);
+    return dateView.getDate() + " th" + (dateView.getMonth() + 1) + ", " + dateView.getFullYear();
 }
