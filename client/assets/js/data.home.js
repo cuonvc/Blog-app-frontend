29761eb0c6e4ed3e6040e13db90e1841b4b003de
cuonvc <cuong.fithau@gmail.com>
2022-08-06 22:35:10 +0700

    login to home page by user

--- client/assets/js/data.home.js
+++ client/assets/js/data.home.js
@@ -1,8 +1,49 @@
+// console.log(localStorage.getItem("accessToken"));
+
+var urlString = window.location.href;
+var urlObj = new URL(urlString);
+var username = urlObj.searchParams.get("u");  //get username from param
+
+renderHeaderInfo();
 renderCategories();
 renderPostsPin();
 renderPostsNew();
 
-// console.log(localStorage.getItem("accessToken"));
+function renderHeaderInfo() {
+    fetch(`http://localhost:8080/api/v1/profile/${username}`)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        const userBox = document.querySelector(".navbar-user");
+        const userContent = `
+            <div class="navbar-user_btn row no-gutters">
+                <div class="navbar-user_avt">
+                    <img src="${data.avatarPhoto}" alt="avt">
+                </div>
+                <div class="navbar-user_name">
+                    <span>${data.firstName} ${data.lastName}</span>
+                </div>
+                <div class="navbar-user_icon-down">
+                    <i class="fa-solid fa-caret-down"></i>
+                </div>
+            </div>
+            <ul class="navbar-user_box">
+                <li class="navbar-user_detail">
+                    <a href="#" class="navbar-user_link">Thông tin tài khoản</a>
+                </li>
+                <li class="navbar-user_posts">
+                    <a href="#" class="navbar-user_post-link">Bài viết của bạn</a>
+                </li>
+                <li class="navbar-logout logout_btn row no-gutters">
+                    <span class="navbar-logo_btn">Đăng xuất</span>
+                    <i class="fa-solid fa-right-from-bracket"></i>
+                </li>
+            </ul>
+        `
+        userBox.innerHTML = userContent;
+    });
+}
 
 function renderCategories() {
     fetch('http://localhost:8080/api/v1/categories')
@@ -139,7 +180,7 @@ function renderPostsNew() {
     });
 }
 
-function renderPostsPaging() {
+function renderPagingPosts() {
     
 }
 
