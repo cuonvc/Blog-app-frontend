03ee2cb6400a3168508528d5344865729ef7ecfa
cuonvc <cuong.fithau@gmail.com>
2022-08-09 18:09:56 +0700

    get data default write page

---
+++ client/assets/js/data.wirte.js
@@ -0,0 +1,74 @@
+function parseJwt(token) {
+    if (!token) { 
+        return;
+    }
+    const base64Url = token.split('.')[1];
+    const base64 = base64Url.replace('-', '+').replace('_', '/');
+    return JSON.parse(window.atob(base64));
+}
+
+const username = parseJwt(localStorage.getItem("accessToken")).sub;  //get username from token
+
+renderHeaderInfo();
+renderCategories();
+
+function renderHeaderInfo() {
+    fetch(`http://localhost:8080/api/v1/profile/${username}`)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        const userBox = document.querySelector(".navbar-user");
+        const userContent = `
+            <div class="navbar-user_btn row no-gutters">
+                <div class="navbar-user_avt">
+                    <img src="${data.avatarPhoto}" alt="avt">
+                </div>
+                <div class="navbar-user_name">
+                    <span>${data.firstName} ${data.lastName}</span>
+                </div>
+                <div class="navbar-user_icon-down">
+                    <i class="fa-solid fa-caret-down"></i>
+                </div>
+            </div>
+            <ul class="navbar-user_box">
+                <li class="navbar-user_detail">
+                    <a href="#" class="navbar-user_link">Thông tin tài khoản</a>
+                </li>
+                <li class="navbar-user_posts">
+                    <a href="#" class="navbar-user_post-link">Bài viết của bạn</a>
+                </li>
+                <li class="navbar-logout logout_btn-home row no-gutters">
+                    <span class="navbar-logo_btn">Đăng xuất</span>
+                    <i class="fa-solid fa-right-from-bracket"></i>
+                </li>
+            </ul>
+        `
+        userBox.innerHTML = userContent;
+
+        logoutAccount(document.querySelector(".logout_btn-home"));
+    });
+}
+
+function renderCategories() {
+    fetch('http://localhost:8080/api/v1/categories')
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(categories) {
+        let htmls = "";
+        categories.map(category => {
+            let html = `<li class="option-category_item">${category.name}</li>`;
+            htmls += html;
+        });
+
+        document.querySelector(".option-categories_available").innerHTML = htmls;
+    });
+}
+
+function logoutAccount(logoutBtn) {
+    logoutBtn.addEventListener("click", function() {
+        localStorage.clear();
+        window.location.href = "../guest/index.html";
+    });
+}
--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -1,35 +1,38 @@
 // select categories
 
-var categoryAvailableList = document.querySelectorAll(".option-category_item");
+window.addEventListener("load", function() {
 
-const ulCategoriesAvailable = document.querySelector(".option-categories_available");
-const ulCategoriesSelect = document.querySelector(".option-categories_select");
+    var categoryAvailableList = document.querySelectorAll(".option-category_item");
     
+    const ulCategoriesAvailable = document.querySelector(".option-categories_available");
+    const ulCategoriesSelect = document.querySelector(".option-categories_select");
 
-for (let i = 0; i < categoryAvailableList.length; i++) {
-    categoryAvailableList[i].addEventListener("click", function() {
-        ulCategoriesSelect.appendChild(categoryAvailableList[i]);
-        
-        var itemSelected = ulCategoriesSelect
-            .children[ulCategoriesSelect.childElementCount - 1];
-        
-        itemSelected.classList.add("selected");
-
-
-        var categorySelectedList = document.querySelectorAll(".selected");
+    for (let i = 0; i < categoryAvailableList.length; i++) {
+        categoryAvailableList[i].addEventListener("click", function() {
+            ulCategoriesSelect.appendChild(categoryAvailableList[i]);
+            
+            var itemSelected = ulCategoriesSelect
+                .children[ulCategoriesSelect.childElementCount - 1];
+            
+            itemSelected.classList.add("selected");
     
-        for (let j = 0; j < categorySelectedList.length; j++) {
-            categorySelectedList[j].addEventListener("click", function() {
-                ulCategoriesAvailable.appendChild(categorySelectedList[j]);
+    
+            var categorySelectedList = document.querySelectorAll(".selected");
         
-                var item = ulCategoriesAvailable
-                    .children[ulCategoriesAvailable.childElementCount - 1];
-        
-                item.classList.remove("selected");
-            });
-        }
-    });
-}
+            for (let j = 0; j < categorySelectedList.length; j++) {
+                categorySelectedList[j].addEventListener("click", function() {
+                    ulCategoriesAvailable.appendChild(categorySelectedList[j]);
+            
+                    var item = ulCategoriesAvailable
+                        .children[ulCategoriesAvailable.childElementCount - 1];
+            
+                    item.classList.remove("selected");
+                });
+            }
+        });
+    }
+});
+
 
 
 var modalOptionBtn = document.querySelector(".body-option_btn");
@@ -50,14 +53,14 @@ modalCancelBtn.addEventListener("click", function() {
 const imageInput = document.querySelector("#img-background_file");
 var uploadImage = "";
 
-imageInput.addEventListener("change", () => {
-    console.log(imageInput.value);
-    const fileReader = new FileReader();
-    fileReader.addEventListener("load", () => {
-        uploadImage = fileReader.result;
-        document.querySelector("#image-background_display")
-            .style.backgroundImage = `url(${uploadImage})`;
-    });
+// imageInput.addEventListener("change", () => {
+//     console.log(imageInput.value);
+//     const fileReader = new FileReader();
+//     fileReader.addEventListener("load", () => {
+//         uploadImage = fileReader.result;
+//         document.querySelector("#image-background_display")
+//             .style.backgroundImage = `url(${uploadImage})`;
+//     });
 
-    fileReader.readAsDataURL(this.files[0]);
-});
+//     fileReader.readAsDataURL(this.files[0]);
+// });
